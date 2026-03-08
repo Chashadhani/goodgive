72af801434337d60ce01433136134426a239869d
chashadhani <chashadhanianjala@gmail.com>
2026-03-09 00:04:56 +0530

    favicon update

--- resources/views/users/my-requests.blade.php
+++ resources/views/users/my-requests.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/jpeg" href="{{ asset('favicon/favicon.jpeg') }}">
     <title>My Requests - {{ config('app.name', 'GoodGive') }}</title>
 
     <link rel="preconnect" href="https://fonts.bunny.net">
